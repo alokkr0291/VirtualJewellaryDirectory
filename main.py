f655ab80ffc0ba9fffe3126c466e1f776c1cc6e4
subhama <subhama@moback.com>
2023-11-24 13:16:04 +0530

    socket added

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import Flask, render_template, Response
 import cv2
 import FaceDetector as detector
+from server import imageObject
 import Augmentation as ag
 
 app=Flask(__name__)
-camera = cv2.VideoCapture(0)
+
 
 
 @app.route('/')
@@ -13,7 +14,7 @@ def index():
 
 @app.route('/video_feed')
 def video_feed():
-    return Response(detector.PlaceObject("alok1.png"), mimetype='multipart/x-mixed-replace; boundary=frame')
+    return Response(imageObject("alok1.png"), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__=='__main__':
-    app.run(host='0.0.0.0', port=80,debug=False)
+    app.run(host='0.0.0.0', port=8080,debug=True)
