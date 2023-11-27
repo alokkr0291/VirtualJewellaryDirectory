300fc5026b48fdc4f94e6a9b46cef48bd5d1949e
subhama <subhama@moback.com>
2023-11-27 14:43:51 +0530

    image_trial trial

--- app.py
+++ app.py
@@ -1,40 +1,37 @@
-
-
-from flask import Flask, render_template, Response
 import cv2
-app=Flask(__name__)
-camera = cv2.VideoCapture(0)
+import base64
+import io
+from PIL import Image
+import numpy as np
+from flask_socketio import emit, SocketIO
+from flask import Flask, render_template
 
-#
-def gen_frames():
-    while True:
-        success, frame = camera.read()  # read the camera frame
-        if not success:
-            break
-        else:
-            detector=cv2.CascadeClassifier('Haarcascades/haarcascade_frontalface_default.xml')
-            eye_cascade = cv2.CascadeClassifier('Haarcascades/haarcascade_eye.xml')
-            faces=detector.detectMultiScale(frame,1.1,7)
-            gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-             #Draw the rectangle around each face
-            for (x, y, w, h) in faces:
-                cv2.rectangle(frame, (x, y), (x+w, y+h), (255, 0, 0), 2)
-                roi_gray = gray[y:y+h, x:x+w]
-                roi_color = frame[y:y+h, x:x+w]
-                eyes = eye_cascade.detectMultiScale(roi_gray, 1.1, 3)
-                for (ex, ey, ew, eh) in eyes:
-                    cv2.rectangle(roi_color, (ex, ey), (ex+ew, ey+eh), (0, 255, 0), 2)
+app = Flask(__name__, template_folder="templates")
+sio = SocketIO(app)
 
-            ret, buffer = cv2.imencode('.jpg', frame)
-            frame = buffer.tobytes()
-            yield (b'--frame\r\n'
-                   b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
-#
-# @app.route('/')
-# def index():
-#     return render_template('index.html')
-# @app.route('/video_feed')
-# def video_feed():
-#     return Response(gen_frames(), mimetype='multipart/x-mixed-replace; boundary=frame')
-# if __name__=='__main__':
-#     app.run(debug=True)
+@sio.on('image')
+def image(data_image):
+    sbuf = io.StringIO()
+    sbuf.write(data_image)
+    b = io.BytesIO(base64.b64decode(data_image))
+    pimg = Image.open(b)
+
+    # DO WHATEVER IMAGE PROCESSING HERE{
+    frame = cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
+    frame = cv2.flip(frame, flipCode=0)
+    imgencode = cv2.imencode('.jpg', frame)[1]
+    #}
+
+    stringData = base64.b64encode(imgencode).decode('utf-8')
+    b64_src = 'data:image/jpeg;base64,'
+    stringData = b64_src + stringData
+    emit('response_back', stringData)
+
+@app.route("/")
+@app.route("/home")
+def index():
+    return render_template("index.html")
+
+
+if __name__ == "__main__":
+    sio.run(app, debug=True)
